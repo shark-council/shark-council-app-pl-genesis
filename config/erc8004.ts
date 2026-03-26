cbbcf5634ccf2b2fbff6ea0de3fa825e6df59f66
kiv1n <aloha@kiv1n.ru>
2026-03-26 18:23:17 +0300

    Update minCreatedAt date in erc8004Config to reflect new deadline

--- config/erc8004.ts
+++ config/erc8004.ts
@@ -3,5 +3,5 @@ import { baseSepolia } from "viem/chains";
 export const erc8004Config = {
   chain: baseSepolia,
   explorer: "https://testnet.8004scan.io/agents/base-sepolia",
-  minCreatedAt: new Date("2026-03-12T17:00:00+03:00"),
+  minCreatedAt: new Date("2026-03-26T17:00:00+03:00"),
 };
